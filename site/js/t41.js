eebf95ce5a417f1bc54db3e63339369c02d19d74
chalaschek <chalaschek@gmail.com>
2013-04-18 18:41:28 -0700

    Initial commit

---
+++ site/js/t41.js
@@ -0,0 +1,14 @@
+(function(){
+
+  $(document).ready(function(){
+    $('.vimeo').fancybox({
+      openEffect  : 'none',
+      closeEffect : 'none',
+      helpers : {
+        media : {}
+      }
+    });
+  });
+  
+})();
+
