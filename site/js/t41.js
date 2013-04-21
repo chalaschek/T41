8f74835da918abcd06477024e7bcecb341656495
chalaschek <chalaschek@gmail.com>
2013-04-20 20:24:04 -0700

    Video width up'd to 1000px; content auto centered

--- site/js/t41.js
+++ site/js/t41.js
@@ -1,13 +1,28 @@
 (function(){
 
   $(document).ready(function(){
+
+ 
+
+    var updatePos = function () {
+        var el = $("#content");
+        var width = $(window).width();
+        var height = $(window).height();
+        var left = Math.max(0, (width / 2) - (el.width() / 2)) + "px";
+        var top = Math.max(0, (height / 2) - (el.height() / 2)) + "px";
+        el.css("position","absolute").css("left", left).css("top", top);
+    }; 
+    $(window).resize(updatePos);
+    updatePos();
+
+    
     $('.vimeo').fancybox({
       openEffect  : 'none',
       closeEffect : 'none',
       helpers : {
         media : {}
       },
-      width: 900
+      width: 1000
     });
   });
   
